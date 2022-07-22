24a188cff0f4ce3d7019578e3ab95902a4379cb5
Ferroca2 <matheus.ferraz@ga.ita.br>
2022-07-22 07:10:43 -0300

    setup and configurations

--- components/CardFooter.js
+++ components/CardFooter.js
@@ -5,10 +5,10 @@ import { FaSearch } from "react-icons/fa";
 import { GiDiamonds } from "react-icons/gi";
 import { TiMessages } from "react-icons/ti";
 const style = {
-    wrapper: `h-16 w-full flex items-center justify-around py-5 px-4 bg-white`,
-    logo: `object-contain cursor-pointer`,
-    icon: `text-3xl text-gray-400 cursor-pointer`,
-  }
+  wrapper: `h-16 w-full flex items-center justify-around py-5 px-4 bg-white`,
+  logo: `object-contain cursor-pointer`,
+  icon: `text-3xl text-gray-400 cursor-pointer`,
+};
 
 export default function CardFooter() {
   return (
